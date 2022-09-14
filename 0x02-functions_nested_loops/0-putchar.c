791ab38209ca0b2443faa47f7d978cb58a57f510
Viula Boldeesibari <viulamoses@gmail.com>
2022-09-14 14:24:32 -0700

    main.h

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * o-putchar - prints _putchar
+ * @_putchar - this function is been used
+ * Return - 0 is always returned
+ */
+
+int main(void)
+{
+	_putchar ('_');
+	_putchar ('p');
+	_putchar ('u');
+	_putchar ('t');
+	_putchar ('c');
+	_putchar ('h');
+	_putchar ('a');
+	_putchar ('r');
+	_putchar ('\n');
+
+	return (0);
+}
